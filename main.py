37b18139a367e3bdfa624c598b7a4ce6e348cc7c
aoirint <aoirint@gmail.com>
2021-10-06 06:14:32 +0900

    if main

--- main.py
+++ main.py
@@ -1,61 +1,61 @@
-import requests
-from dotenv import load_dotenv
 import os
+import json
+import requests
 
+from dotenv import load_dotenv
 load_dotenv()
 
 TOKEN = os.environ.get('TOKEN')
 assert TOKEN
 
-api_url = 'https://api.github.com/graphql'
+if __name__ == '__main__':
+    api_url = 'https://api.github.com/graphql'
 
-query = """
-query GetLangStats(
-    $login: String!
-) {
-    user(login: $login) {
-        repositories(
-            ownerAffiliations: OWNER
-            isFork: false
-            first: 100
-        ) {
-            nodes {
-                languages(
-                    first: 10
-                    orderBy: {
-                        field: SIZE
-                        direction: DESC
-                    }
-                ) {
-                    edges {
-                        size
-                        node {
-                            color
-                            name
+    query = """
+    query GetLangStats(
+        $login: String!
+    ) {
+        user(login: $login) {
+            repositories(
+                ownerAffiliations: OWNER
+                isFork: false
+                first: 100
+            ) {
+                nodes {
+                    languages(
+                        first: 10
+                        orderBy: {
+                            field: SIZE
+                            direction: DESC
+                        }
+                    ) {
+                        edges {
+                            size
+                            node {
+                                color
+                                name
+                            }
                         }
                     }
                 }
             }
         }
     }
-}
-"""
+    """
 
-payload = {
-    'query': query,
-    'variables': {
-        'login': 'aoirint',
-    },
-}
+    payload = {
+        'query': query,
+        'variables': {
+            'login': 'aoirint',
+        },
+    }
 
-headers = {
-    'Content-Type': 'application/json',
-    'Authorization': f'Bearer {TOKEN}'
-}
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {TOKEN}'
+    }
 
-import json
-res = requests.post(api_url, headers=headers, data=json.dumps(payload))
+    res = requests.post(api_url, headers=headers, data=json.dumps(payload))
 
-print(res.status_code)
-print(res.json())
-
+    print(res.status_code)
+    print(res.json())
